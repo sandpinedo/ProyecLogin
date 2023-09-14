0cfb6ccf65b25029d73a171a37943956152aaf1d
sandpinedo <1440690@senati.pe>
2023-09-14 12:33:13 -0500

    empece a crear la base de datos

---
+++ Models/DbpruebaContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProLogin.Models;
+
+public partial class DbpruebaContext : DbContext
+{
+    public DbpruebaContext()
+    {
+    }
+
+    public DbpruebaContext(DbContextOptions<DbpruebaContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
